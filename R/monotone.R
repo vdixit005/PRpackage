d8b5fbe6f46ad097e02951080495738643e7cf37
vdixit005 <vdixit@ncsu.edu>
2022-06-12 12:05:17 -0400

    Added monotone plots

--- R/monotone.R
+++ R/monotone.R
@@ -20,7 +20,7 @@
 #'m = monotone(X)
 #'# Plot with the truth
 #'truth = 2*dnorm(m$Xsup)
-#'plot(m$Xsup, truth, type = "l")
+#'plot(m)
 #'lines(m$Xsup, m$m, col = "red")
 
 #' @references
@@ -35,7 +35,7 @@ f0 = dunif(U, min = min(U), max = max(U))
 Xsup = seq(10^-20, max(X), length.out = 101)
 f = pr(X = X, d = d.mon, U = U, f0 = f0, nperm = nperm)
 m = mixture_density(f = f$f, U = U, d = d.mon, Xsup = Xsup)
-return(list(Xsup = Xsup, m = m))
+return(m)
 }
 #'
 d.mon = function(x,u){
